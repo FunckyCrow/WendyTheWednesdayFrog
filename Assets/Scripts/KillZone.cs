8305287f0180b73c5f2fe97018683540318cca33
Maxime Morin Grandmont <Maxime.mg.11@gmail.com>
2021-12-17 15:20:09 -0500

    KillZone added

---
+++ Assets/Scripts/KillZone.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class KillZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        CharacterController wendy = other.gameObject.GetComponent<CharacterController>();
+        //if (wendy) wendy.Die();
+
+        StartCoroutine(DieCoroutine());
+    }
+
+    private IEnumerator DieCoroutine()
+    {
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
